d5035faa0add41a388b940ce4d332d1c6402f2af
hsauers5@gmail.com <hsauers5@gmail.com>
2022-04-12 13:19:33 -0400

    Some changes to the model

--- main.py
+++ main.py
@@ -17,21 +17,22 @@ def main():
         prime_rate = float(row['PrimeRate'])
         shares_out = float(row['SharesOut'])
 
-        # Black-Scholes model
-        S = commodity_price
-        K = production_cost
-        r = prime_rate
-        t = 365
-        v = 48.5
-        annual_option_value_per_ton = op.black_scholes(St=S, K=K, r=r, t=t, v=v)['value']['option value']
-        annual_option_value_to_firm = annual_option_value_per_ton * tons_sold
-        annual_option_value_per_share = annual_option_value_to_firm / shares_out
-
-        # take annual value, discount at prime rate for ten years, get approx. real option value
         real_option_value_per_share = 0
-        discount_rate = prime_rate/100
-        for i in range(1, 11):
-            real_option_value_per_share += annual_option_value_per_share / ((1+discount_rate)**i)
+
+        for i in range(1, 3):
+            # Black-Scholes model
+            S = commodity_price
+            K = production_cost
+            r = prime_rate
+            t = 365 * i
+            v = 40
+            annual_option_value_per_ton = op.black_scholes(St=S, K=K, r=r, t=t, v=v)['value']['option value']
+            annual_option_value_to_firm = annual_option_value_per_ton * 4 * max(tons_sold, 2000)
+            annual_option_value_per_share = annual_option_value_to_firm / shares_out
+
+            # apply discount rate to option value? Kind of already accounted for in B-S
+
+            real_option_value_per_share += annual_option_value_per_share
 
         # print(f'{real_option_value_per_share} | {evps}')
         print(real_option_value_per_share)
